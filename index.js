0c470b741100d706379378ffdcd4b93b97df7384
ellackramer <ellackramer@gmail.com>
2021-08-12 20:43:50 -0400

    commiting final index.js, cleaning up code

---
+++ index.js
@@ -0,0 +1,146 @@
+let inquirer = require('inquirer');
+let fs = require('fs');
+
+inquirer
+    .prompt([
+        {
+            type: "input",
+            message: "What is the title of your project?",
+            name: "title"
+        },
+        {
+            type: "input",
+            message: "Enter a description for your project:",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Enter installation insructions as a comma separated list:",
+            name: "install"
+        },
+        {
+            type: "input",
+            message: "Enter usage information for your project:",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Enter contribution guidelines for your project:",
+            name: "contribution"
+        },
+        {
+            type: "input",
+            message: "Enter test information for your project:",
+            name: "testing"
+        },
+        {
+            type: "input",
+            message: "Enter your GitHub Username:",
+            name: "github"
+        },
+        {
+            type: "input",
+            message: "Enter your email address:",
+            name: "email"
+        },
+        
+    
+    ])
+    .then((res) => {
+        console.log("Creating README file...");
+        createREADMEFile(res);
+        
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+    
+    
+function createREADMEFile(input) {
+    let readmeTitle;
+    let readmeDescription;
+    const descriptionHead = "## Description";
+    let tableOfContents;
+    const tocHead = "## Table of Contents";
+    let installArr;
+    const installHead = "## Installation";
+    let readmeUsage;
+    const usageHead = "## Usage";
+    let readmeContribution;
+    const contributionHead = "## Contribution";
+    let readmeTest;
+    const testingHead = "## Tests";
+    let readmeQuestions;
+    const questionsHead = "## Questions";
+    let completeREADME = [];
+    
+    // Adding Title
+    if (input.title == '') {
+        readmeTitle = '# TITLE';
+    } else {
+        readmeTitle = `# ${input.title}`;
+    }
+    completeREADME.push(readmeTitle);
+    
+        
+    
+    if (input.description == '') {
+        readmeDescription = `${descriptionHead}\n Enter project description here.`;
+    } else {
+        readmeDescription = `${descriptionHead}\n${input.description}`;
+    }
+    completeREADME.push(readmeDescription);
+    
+    
+    tableOfContents = `${tocHead}\n* [Installation](#installation)\n* [Usage](#usage)\n* [Contribution](#contribution)\n* [Tests](#tests)\n* [License](#license)\n* [Questions](#questions)\n`;
+    completeREADME.push(tableOfContents);
+    
+    
+    //Adding installation instructions
+    completeREADME.push(`${installHead}`);
+    
+    installArr = input.install.split(',').map(item => {
+        return `${item.trim()}`;
+    });
+    
+    for (var i = 0; i < installArr.length; i++) {
+        completeREADME.push(`${i + 1}. ${installArr[i]}`);
+    }
+    
+    
+    if (input.usage == '') {
+        readmeUsage = `\n${usageHead}\n Enter project usage here.`;
+    } else {
+        readmeUsage = `\n${usageHead}\n${input.usage}`;
+    }
+    completeREADME.push(readmeUsage);
+    
+    
+    if (input.contribution == '') {
+        readmeContribution = `\n${contributionHead}\n Enter project contriburtion information here.`;
+    } else {
+        readmeContribution = `\n${contributionHead}\n${input.contribution}`;
+    }
+    completeREADME.push(readmeContribution);
+    
+    
+    if (input.testing == '') {
+        readmeTest = `\n${testingHead}\n Enter project testing information here.`;
+    } else {
+        readmeTest = `\n${testingHead}\n${input.testing}`;
+    }
+    completeREADME.push(readmeTest);
+    
+    
+    const README = completeREADME.join('\n');
+        
+    
+    //Creating the README
+    fs.writeFile("./example/README-example.md", README, (err) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("README file successfully created!");
+        }
+    });
+}
